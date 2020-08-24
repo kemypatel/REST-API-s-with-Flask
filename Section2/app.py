4413c42a3056747d41ca8800d6573e5bfdfcb342
Kemi Patel <41269124+kemypatel@users.noreply.github.com>
2020-08-24 15:31:32 +0530

    Add files via upload

---
+++ Section2/app.py
@@ -0,0 +1,67 @@
+from flask import Flask,request
+from flask_restful import Resource,Api,reqparse
+from flask_jwt import JWT,jwt_required
+from security import authenticate, identify
+
+app = Flask(__name__)
+app.secret_key ='kemipatel'
+api = Api(app)
+
+jwt = JWT(app,authenticate,identify) # /auth
+
+items=[]
+
+class Item(Resource):
+     parser = reqparse.RequestParser()
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help="this cannot be left balnk")
+
+       
+
+    @jwt_required()
+    def get(self,name):
+        item = next(filter(lambda x: x['name']==name,items),None)
+        return {'item':item},200 if item else 404
+
+    def post(self,name):
+        
+        if next(filter(lambda x: x['name']==name,items),None):
+            return {'message ':'An item not found '},400
+
+        data = Item.parser.parse_args()
+        
+        data = request.get_json()
+        item ={'name':name,'price':data['price']}
+        items.append(item)
+        return item,201
+
+
+    def delete(self,name):
+        global items
+        items=list(filter(lambda x: x['name'] != name , items))
+        return {'message': 'Item deleted'}
+
+    def put(self,name):
+
+        data =Item.parser.parse_args()
+       
+
+        item = next(filter(lambda x : x['name'] == name,items),None)
+        if item is None:
+            item = {'name':name,'price':data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
+class Items(Resource):
+    def get(self):
+        return {'items':items}
+
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(Items,'/items/')
+
+app.run(port=5000,debug=True)
